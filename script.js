85aea5bc8e4d5d53e02b7ae2ccad0cb7186b70a8
20profmatheus <108628651+20profmatheus@users.noreply.github.com>
2025-07-01 11:37:01 -0300

    aula 0107

---
+++ script.js
@@ -0,0 +1,36 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj,
+        alternativas: [
+            "isso é assutador!",
+            "Isso é maravilhoso",
+        ]
+    },
+    {
+     enunciado:Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj,
+        alternativas: [
+            "isso é assutador!",
+            "Isso é maravilhoso",
+        ]   
+    },
+    {
+      enunciado:Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj,
+        alternativas: [
+            "isso é assutador!",
+            "Isso é maravilhoso",
+        ]     
+    },
+    {
+        enunciado:Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj,
+        alternativas: [
+            "isso é assutador!",
+            "Isso é maravilhoso",
+        ]   
+    },
+]
