18e2497d4f1d8fb811a63914f1aacbf7f5407f88
20profmatheus <108628651+20profmatheus@users.noreply.github.com>
2025-07-29 11:37:45 -0300

    aula 2907

--- script.js
+++ script.js
@@ -6,31 +6,83 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado:Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj,
-        alternativas: [
-            "isso é assutador!",
-            "Isso é maravilhoso",
+        enunciado: "Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj"
+        alternativas: [ 
+            { 
+            texto: "isso é assutador!",
+            afirmacao: "afirmação",
+            },
+            { 
+            texto: "isso é maravilhoso!",
+            afirmacao: "afirmação",
+            },
         ]
     },
+
     {
-     enunciado:Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj,
-        alternativas: [
-            "isso é assutador!",
-            "Isso é maravilhoso",
-        ]   
+        enunciado: "Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj"
+        alternativas: [ 
+            { 
+            texto: "isso é assutador!",
+            afirmacao: "afirmação",
+            },
+            { 
+            texto: "isso é maravilhoso!",
+            afirmacao: "afirmação",
+            },
+        ]
     },
+
     {
-      enunciado:Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj,
-        alternativas: [
-            "isso é assutador!",
-            "Isso é maravilhoso",
-        ]     
+        enunciado: "Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj"
+        alternativas: [ 
+            { 
+            texto: "isso é assutador!",
+            afirmacao: "afirmação",
+            },
+            { 
+            texto: "isso é maravilhoso!",
+            afirmacao: "afirmação",
+            },
+        ]
     },
+
     {
-        enunciado:Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj,
-        alternativas: [
-            "isso é assutador!",
-            "Isso é maravilhoso",
-        ]   
+        enunciado: "Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj"
+        alternativas: [ 
+            { 
+            texto: "isso é assutador!",
+            afirmacao: "afirmação",
+            },
+            { 
+            texto: "isso é maravilhoso!",
+            afirmacao: "afirmação",
+            },
+        ]
     },
-]
+
+    {
+        enunciado: "Assim que saiu da escola vdjkdkjcfjçdfkjdvbjdvj"
+        alternativas: [ 
+            { 
+            texto: "isso é assutador!",
+            afirmacao: "afirmação",
+            },
+            { 
+            texto: "isso é maravilhoso!",
+            afirmacao: "afirmação",
+            },
+        ]
+    },
+ 
+     
+
+let atual =0;
+let perguntaATual;
+
+function mostraPergunta () {
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntas.enunciado;
+}
+
+mostraPergunta ();
